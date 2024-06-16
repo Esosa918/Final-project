39697341a7aec5723f55dc30ea3bbc0ee8b78298
praise <praiseuwadiae3@gmail.com>
2024-06-16 23:48:13 +0100

    second commit

--- src/Cloudimages/Cloud.jsx
+++ src/Cloudimages/Cloud.jsx
@@ -6,27 +6,27 @@ export const Part1 = {
     bedroom: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Browse/skokzwcp2z26gpdtzks1"
 } 
 export const Part2 = {
-    innerPeace:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Rooms/afcjnlihyashai7ma3ue",
-    innerPeace1:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Rooms/vviypggjttpe7iohpazg" 
+    innerPeace: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Rooms/afcjnlihyashai7ma3ue",
+    innerPeace1: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Rooms/vviypggjttpe7iohpazg" 
 }
 export const Part3 = {
-    img1:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/lw3rqfn3bvfpzibnrcbl",
-    img2:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/wveiysovuhi0xlvdop4l",
-    img3:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/j06h5qidgta3bzcgsaeg",
-    img4:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/bizkcnr8aqhm0y164ucv",
-    img5:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/tlyoeanreazmmy4ihubh",
-    img6:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/anoie4dnesvleejqwdn6",
-    img7:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/aqoeh74qtfkkf63aoyum",
-    img8:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/je01cpww3tfgu425m68m",
-    img9:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/dlmda0xbyhz8bwysnowa"
+    img1: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/lw3rqfn3bvfpzibnrcbl",
+    img2: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/wveiysovuhi0xlvdop4l",
+    img3: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/j06h5qidgta3bzcgsaeg",
+    img4: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/bizkcnr8aqhm0y164ucv",
+    img5: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/tlyoeanreazmmy4ihubh",
+    img6: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/anoie4dnesvleejqwdn6",
+    img7: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/aqoeh74qtfkkf63aoyum",
+    img8: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/je01cpww3tfgu425m68m",
+    img9: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Furniture/dlmda0xbyhz8bwysnowa"
 }
 export const Part4 = {
-    syltherine:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/etkjagbgb5u2ubmn4amp",
-    leviosa:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/sovsmlnf5zzhkkgiv3il",
-    lolito:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/ppej9chpm34ul9xr49lj",
-    respira:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/scmbtieyzqwxhtxqwzwm",
-    grifo:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/mlstvjphydi859ecll7g",
-    muggo:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/ucrujeiuq3drlhl1dfho",
-    pingky:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/h0to9vhdjobrzdaqomma",
-    potty:"https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/asc8dbxvilcjzeyvf5gj"
+    syltherine: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/etkjagbgb5u2ubmn4amp",
+    leviosa: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/sovsmlnf5zzhkkgiv3il",
+    lolito: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/ppej9chpm34ul9xr49lj",
+    respira: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/scmbtieyzqwxhtxqwzwm",
+    grifo: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/mlstvjphydi859ecll7g",
+    muggo: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/ucrujeiuq3drlhl1dfho",
+    pingky: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/h0to9vhdjobrzdaqomma",
+    potty: "https://res.cloudinary.com/dn61rvaob/image/upload/f_auto,q_auto/v1/Project%20E-C/Our%20product/asc8dbxvilcjzeyvf5gj"
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,71 +1,112 @@
 import React from 'react'
-
-
+import { Image } from  "cloudinary-react"
+import { Part1, Part2, Part3, Part4, cloudName } from '../Cloudimages/Cloud'
+import './Home.css'
 
 const Home = () => {
   return (
     <div>
-        <div>
-            <div>
-                <p>New Arrival</p>
-                <h2>Discover Our <br></br> New Collection </h2>
-                <p></p>
+        <div className='left'> 
+            <div className='left2'>
+                <div className='left3'>
+                    <p className='l1'>New Arrival</p>
+                    <h2 className='l2'>Discover Our <br></br> New Collection </h2>
+                    <p className='l3'></p>
+                    <button className='yellow'>Buy Now</button>
+                </div>
             </div>
         </div>
-        <section>
+        <section className='front'>
             <h3>Browse The Range</h3>
             <p></p>
-            <div>
-                
+            <div className='front1'>
+                <Image cloudName={cloudName} publicId={Part1.dining}></Image>
+                <Image cloudName={cloudName} publicId={Part1.living}></Image>
+                <Image cloudName={cloudName} publicId={Part1.bedroom}></Image>
+            </div>
+            <div className='front2'>
+                <p>Dining</p>
+                <p>living</p>
+                <p>Bedroom</p>
             </div>
         </section>
-        <section>
+        <section className='red'>
             <h2>Our Products</h2>
-            <div>
-                <div>
-                    <p>Syltherine</p>
-                    <p></p>
-                    <p></p>
+            <div className='red1'>
+                <div className='red2'>
+                    <Image cloudName={cloudName} publicId={Part4.syltherine}></Image>
+                    <p className='blue'>Syltherine</p>
+                    <p><span className='white'>Stylish cafe chair</span></p>
+                    <p className='white1'>Rp 2.500.000<span className='white2'>Rp 3.500.000</span></p>
                 </div>
-                <div>
-                    <p>Laviosa</p>
-                    <p></p>
-                    <p></p>
+                <div className='red2'>
+                    <Image cloudName={cloudName} publicId={Part4.leviosa}></Image>
+                    <p className='blue'>Laviosa</p>
+                    <p><span className='white'>Stylish cafe chair</span></p>
+                    <p className='white1'>Rp 2.500.000</p>
                 </div>
-                <div>
-                    <p>Lolito</p>
-                    <p></p>
-                    <p></p>
+                <div className='red2'>
+                    <Image cloudName={cloudName} publicId={Part4.lolito}></Image>
+                    <p className='blue'>Lolito</p>
+                    <p><span className='white'>Luxury big sofa</span></p>
+                    <p className='white1'>Rp 7.000.000<span className='white2'>Rp 14.000.000</span></p>
                 </div>
-                <div>
-                    <p>Respira</p>
-                    <p></p>
-                    <p></p>
+                <div className='red2' >
+                    <Image cloudName={cloudName} publicId={Part4.respira}></Image>
+                    <p className='blue'>Respira</p>
+                    <p><span className='white'>Outdoor bar table and stool</span></p>
+                    <p className='white1'>Rp 500.000</p>
                 </div>
             </div>
-            <div>
-                <div>
-                    <p>Grifo</p>
-                    <p></p>
-                    <p></p>
+            <div className='red1'>
+                <div className='red2'>
+                    <Image cloudName={cloudName} publicId={Part4.grifo}></Image>
+                    <p className='blue'>Grifo</p>
+                    <p><span className='white'>Night lamp</span></p>
+                    <p className='white1'>Rp 1.500.000</p>
                 </div>
-                <div>
-                    <p>Muggo</p>
-                    <p></p>
-                    <p></p>
+                <div className='red2'>
+                    <Image cloudName={cloudName} publicId={Part4.muggo}></Image>
+                    <p className='blue'>Muggo</p>
+                    <p><span className='white'>Small mug</span></p>
+                    <p className='white1'>Rp 7.000.000</p>
                 </div>
-                <div>
-                    <p>Pingky</p>
-                    <p></p>
-                    <p></p>
+                <div className='red2'>
+                    <Image cloudName={cloudName} publicId={Part4.pingky}></Image>
+                    <p className='blue'>Pingky</p>
+                    <p><span className='white'>Cute bed set</span></p>
+                    <p className='white1'><span className='white2'>Rp 14.000.000</span></p>
                 </div>
-                <div>
-                    <p>Potty</p>
-                    <p></p>
-                    <p></p>
+                <div className='red2'>
+                    <Image cloudName={cloudName} publicId={Part4.potty}></Image>
+                    <p className='blue'>Potty</p>
+                    <p><span className='white'>MInimalist flower pot</span></p>
+                    <p className='white1'>Rp 500.000</p>
                 </div>
             </div>
             <button>Show More </button>
+        </section>
+        <section>
+            <div></div>
+            <div>
+                <Image cloudName={cloudName} publicId={Part2.innerPeace}></Image>
+                <Image cloudName={cloudName} publicId={Part2.innerPeace1}></Image>
+
+            </div>
+        </section>
+        <section>
+            <div>
+                <Image cloudName={cloudName} publicId={Part3.img1}></Image>
+                <Image cloudName={cloudName} publicId={Part3.img2}></Image>
+                <Image cloudName={cloudName} publicId={Part3.img3}></Image>
+                <Image cloudName={cloudName} publicId={Part3.img4}></Image>
+                <Image cloudName={cloudName} publicId={Part3.img5}></Image>
+                <Image cloudName={cloudName} publicId={Part3.img6}></Image>
+                <Image cloudName={cloudName} publicId={Part3.img7}></Image>
+                <Image cloudName={cloudName} publicId={Part3.img8}></Image>
+                <Image cloudName={cloudName} publicId={Part3.img9}></Image>
+
+            </div>
         </section>
     </div>
   )
